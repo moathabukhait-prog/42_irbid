9440b053c915dc040a83dd7fd7de49a8901f426e
mabukhai mabukhai <mabukhai@c1r4s7.irbid.42.school>
2026-09-08 13:49:09 +0300

    uu

---
+++ libft/ft_memset.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mabukhai <mabukhai@learner.42.tech>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/09/07 12:45:34 by mabukhai          #+#    #+#             */
+/*   Updated: 2026/09/07 14:23:05 by mabukhai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memset(void *s, int v, size_t num)
+{
+	unsigned char	*ptr;
+	size_t			i;
+
+	ptr = (unsigned char *)s;
+	i = 0;
+	while (i < num)
+	{
+		ptr[i] = (unsigned char)v;
+		i++;
+	}
+	return (s);
+}
